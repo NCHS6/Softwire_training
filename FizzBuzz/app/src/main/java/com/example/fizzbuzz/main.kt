b9f255d4b1dc9c190b1ea26cfd21ef47366d1c65
NCHS6 <nickleby.simmonds@gmail.com>
2025-07-02 10:17:42 +0100

    latest

--- FizzBuzz/app/src/main/java/com/example/fizzbuzz/main.kt
+++ FizzBuzz/app/src/main/java/com/example/fizzbuzz/main.kt
@@ -2,44 +2,46 @@ package com.example.fizzbuzz
 import java.util.Scanner
 
 fun main() { //change to commit
-    print("Enter a number: ")
+    print("Enter max number: ")
     val scanner = Scanner(System.`in`)
-    val num:Int = scanner.nextLine().toInt()
-    print("Enter rule numbers: ")
+    val max: Int = scanner.nextLine().toInt()
+    print("Enter rule numbers separated with a space: ")
     val rules = scanner.nextLine().split(' ')
+    (1..max).forEach {  num ->
+        var outString: String = ""
+        if (num % 3 == 0 && "3" in rules) {
+            outString += "Fizz"
+        }
+        if (num % 5 == 0 && "5" in rules) {
+            outString += "Buzz"
+        }
+        if (num % 7 == 0 && "7" in rules) {
+            outString += "Bang"
+        }
+        if (num % 11 == 0 && "11" in rules) {
+            outString = "Bong"
+        }
+        if (num % 13 == 0 && "13" in rules) {
+            val bIndex:Int = outString.indexOf('B')
+            if (bIndex == -1) {
+                outString += "Fezz"
+            } else {
+                outString = outString.subSequence(0,bIndex).toString() + "Fezz" + outString.substring(bIndex)
+            }
 
-    var out_string: String = ""
-    if (num % 3 == 0 && "3" in rules) {
-        out_string += "Fizz"
-    }
-    if (num % 5 == 0 && "5" in rules) {
-        out_string += "Buzz"
-    }
-    if (num % 7 == 0 && "7" in rules) {
-        out_string += "Bang"
-    }
-    if (num % 11 == 0 && "11" in rules) {
-        out_string = "Bong"
-    }
-    if (num % 13 == 0 && "13" in rules) {
-        val b_index:Int = out_string.indexOf('B')
-        if (b_index == -1) {
-            out_string += "Fezz"
-        } else {
-            out_string = out_string.subSequence(0,b_index).toString() + "Fezz" + out_string.substring(b_index)
+        }
+        if (num % 17 == 0 && "17" in rules) {
+            val reversed = outString.chunked(4).reversed()
+            outString = reversed.joinToString(separator = "")
         }
 
-    }
-    if (num % 17 == 0 && "17" in rules) {
-        val reversed = out_string.chunked(4).reversed()
-        out_string = reversed.joinToString(separator = "")
+        if (outString == "") {
+            outString = num.toString()
+        }
+
+        print("\n" + outString)
     }
 
-    if (out_string == "") {
-        out_string = num.toString()
-    }
-
-    print(out_string)
 
 
 
